b8487b94199848bacc1a30ac59f366cfffe714c1
Руслан Аитов <r_aitov@mail.ru>
2023-06-10 14:27:39 +0300

    разборы

--- task19-21/2k-mult.py
+++ task19-21/2k-mult.py
@@ -1,21 +1,22 @@
-start = 4
-win = 140
-
-
-def f(a, b, c, m):
-    if a * b >= win:
+def f(s1, s2, c, m):
+    # if s > 80:
+    #     return c % 2 != m % 2
+    if s1 + s2 >= 77:
         return c % 2 == m % 2
     if c == m:
         return False
-    moves = [f(a + 1, b, c + 1, m),
-             f(a * 2, b, c + 1, m),
-             f(a, b + 1, c + 1, m),
-             f(a, b * 2, c + 1, m)]
-    return any(moves) if (c + 1) % 2 == m % 2 else all(moves)
+    moves = [f(s1 + 1, s2, c + 1, m),
+             f(s1 * 2, s2, c + 1, m),
+             f(s1, s2 + 1, c + 1, m),
+             f(s1, s2 * 2, c + 1, m)]
+    if (c + 1) % 2 == m % 2:
+        return any(moves)
+    else:
+        return all(moves)
 
 
-for s in range(1, 34):
+for s in range(1, 70):
     for m in range(1, 5):
-        if f(start, s, 0, m):
+        if f(7, s, 0, m):
             print(s, m)
             break
